d496a9b3f1ac8d7addb85c5eb15d06da564170a5
Carlos Correa <carlosdcorrea3@gmail.com>
2024-06-03 22:10:39 -0500

    Changes in category selection
    Getting categories from backend done and issues with getting the value of the categorySelected retrieve by the parent view (expenses\create) due to queryParam naming and duplication naming solve

--- app/(app)/category.tsx
+++ app/(app)/category.tsx
@@ -4,53 +4,61 @@ import { View, Pressable, StyleSheet } from 'react-native';
 import { Link, useLocalSearchParams } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
 import { ClashGroteskBold, ClashGrotesk } from 'components/text/StyledText';
+import { serverUrl } from 'constants/server'
 
 
 type category = {
-  id: string,
+  category_id: string,
   name: string,
-  subCategory: string | undefined
+  icon: keyof typeof AntDesign.glyphMap
 };
 
 
+function listCategories(setCategories: any): void {
+  let queryParams = {
+
+  }
+
+  fetch(`${serverUrl}/category/list`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Token 6b6e12cf2e3732506a0811ecd2703b958025d190'
+    }
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return response.text().then(text => { throw new Error(text) })
+    })
+    .then(data => {
+      setCategories(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
 export default function CategorySelect() {
-  const [categories, setCategories] = useState<category[]>([
-    {
-      id: '1',
-      name: 'cat1',
-      subCategory: undefined
-    },
-    {
-      id: '2',
-      name: 'cat1',
-      subCategory: undefined
-    },
-    {
-      id: '3',
-      name: 'cat1',
-      subCategory: undefined
-    },
-    {
-      id: '4',
-      name: 'cat1',
-      subCategory: undefined
-    },
-    {
-      id: '5',
-      name: 'cat1',
-      subCategory: undefined
-    },
-  ]);
+  const [categories, setCategories] = useState<category[]>();
   const [categorySelectedId, setCategorySelectedId] = useState<string | undefined>();
-  const { category } = useLocalSearchParams<{category: string}>();
+  const { categorySelectedIdParam } = useLocalSearchParams<{ categorySelectedIdParam: string }>();
 
   useEffect(() => {
-    setCategorySelectedId(category);
-  }, [])
+    console.log('being called');
+    listCategories(setCategories);
+    setCategorySelectedId(categorySelectedIdParam);
+    if (categories) {
+      console.log(categories.length);
+    }
+    console.log(`categorySelected ${categorySelectedId}`)
+  }, []);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
@@ -58,18 +66,18 @@ export default function CategorySelect() {
         <ClashGroteskBold style={{ fontSize: 30, color: '#8b4513' }}>Seleccionar una categoria</ClashGroteskBold>
       </View>
       <View style={{ flexDirection: 'row', flexWrap: 'wrap', alignContent: 'center', justifyContent: 'center' }}>
-        {categories.map((category) => (
+        {categories && categories.map((category) => (
           <Pressable onPress={(_) => {
-            if (categorySelectedId === category.id) {
+            if (categorySelectedId === category.category_id) {
               setCategorySelectedId(undefined);
             } else {
-              setCategorySelectedId(category.id);
+              setCategorySelectedId(category.category_id);
             }
           }}>
             <View
-              key={category.id}
+              key={category.category_id}
               style={{
-                backgroundColor: categorySelectedId === category.id ? '#ff8c00': 'transparent',
+                backgroundColor: categorySelectedId === category.category_id ? '#ff8c00' : 'transparent',
                 justifyContent: 'center',
                 alignItems: 'center',
                 width: 100,
@@ -79,8 +87,8 @@ export default function CategorySelect() {
                 borderWidth: 0.5,
                 borderRadius: 10
               }}>
-              <AntDesign name="home" size={30} color={categorySelectedId === category.id ? "#fffaf0": "black"} />
-              <ClashGrotesk style={{ fontSize: 20, color: categorySelectedId === category.id ? "#fffaf0": "black" }}>{category.name}</ClashGrotesk>
+              <AntDesign name={category.icon} size={30} color={categorySelectedId === category.category_id ? "#fffaf0" : "black"} />
+              <ClashGrotesk style={{ fontSize: 20, color: categorySelectedId === category.category_id ? "#fffaf0" : "black" }}>{category.name}</ClashGrotesk>
             </View>
           </Pressable>
         ))}
@@ -90,7 +98,7 @@ export default function CategorySelect() {
         style={{ alignSelf: 'center', marginTop: 30 }}
         href={{
           pathname: '/expenses/create',
-          params: categorySelectedId ? { category: categorySelectedId } : undefined
+          params: categorySelectedId ? { categorySelectedIdParam: categorySelectedId } : undefined
         }}>
         {categorySelectedId === undefined ? (
           <Ionicons name="arrow-back-sharp" size={30} color="#ff4500" />
